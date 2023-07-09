8a57b4494922c8d2060dedc4e506039be7133885
Fabián Huertas <miloh9318@hotmail.com>
2023-07-09 16:52:05 -0500

    agregando metodo Math y la figura circulo

--- math.js
+++ math.js
@@ -1,4 +1,4 @@
-console.group(cuadrado);
+console.group('cuadrado');
 
 const ladoCuadrado = 5;
 const perimetroCuadrado = ladoCuadrado * 4;
@@ -17,9 +17,9 @@ function calcularCuadrado (lado){
     };
 }
 
-console.endGroup(cuadrado);
+console.groupEnd('cuadrado')
 
-console.group(triangulo);
+console.group('triangulo');
 
 const ladoTriangulo1 = 6;
 const ladoTriangulo2 = 6;
@@ -45,4 +45,33 @@ perimetroTriangulo,
 areaTriangulo,
 });
 
-console.endGroup(triangulo);
+console.groupEnd('triangulo')
+
+console.group('circulo');
+
+const radioCirculo = 3;
+const diametroCirculo = radioCirculo * 2;
+
+
+const circunferenciaCirculo = diametroCirculo * Math.PI;
+const areaCirculo = (radioCirculo ** 2) * Math.PI;
+
+console.log({
+    radioCirculo,
+    diametroCirculo,
+    circunferenciaCirculo,
+    areaCirculo,
+});
+
+function calcularCirculo (radio) {
+    const diametro = radio * 2;
+    const radioAlCuadrado = Math.pow(radio, 2);
+
+    return {
+        circunferencia: diametro * Math.PI,
+        area : radioAlCuadrado * Math.PI,
+    };
+}
+
+console.groupEnd('circulo')
+
