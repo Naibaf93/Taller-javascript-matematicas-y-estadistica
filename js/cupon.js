1c5311747207cdc17afa87166f15b3a6554b42b0
Fabián Huertas <miloh9318@hotmail.com>
2023-08-04 20:18:07 -0500

    agregando metodo find para validacion

--- js/cupon.js
+++ js/cupon.js
@@ -7,6 +7,36 @@ btn.addEventListener('click' , calcularPrecioConDescuento);
 
 const arrayUObjeto = undefined; //['cupones' : descuento] {}?
 
+
+/*
+--------UTILIZANDO OBJETOS-------
+const couponsObject = {
+    'cupon1':30,
+    'cupon2':25,
+    'cupon3':20,
+};
+
+*/
+
+/*------UTILIZANDO ARRAYS------*/
+
+const couponArray = [];
+couponArray.push({
+  name: 'descuento30',
+  discount: 30, 
+})
+couponArray.push({
+    name: 'descuento25',
+    discount: 25, 
+})
+couponArray.push({
+    name: 'descuento20',
+    discount: 20, 
+})
+couponArray.push({
+    name: 'descuento15',
+    discount: 15, 
+})
 function calcularPrecioConDescuento() { 
     // (P * (100 - D)) / 100
 
@@ -18,9 +48,41 @@ function calcularPrecioConDescuento() {
         return;    
     }
 
+    function isCouponInArray(couponElement) {
+        return couponElement.name == coupon;
+      }
+      console.log(isCouponInArray);
+
+      /*utilizamos el método .find para preguntarle al array couponArray si alguno de los objetos con propiedad 'name' coincide con lo que escribieron los usuarios en inputCoupon */  
+
+    const couponInArray = couponArray.find(isCouponInArray);
+    console.log(couponInArray); 
+
     let discount;
 
-    if (coupon == 'fabian_es_spiderman') {
+    if (couponInArray) {
+        discount = couponInArray.discount;
+      } else {
+        priceResult.innerText = 'El cupón no es valido';
+        return;
+      }
+
+/*
+-------CONDICIONAL USANDO OBJETOS-------------
+    if (couponsObject[coupon]) {
+        discount = couponsObject[coupon];
+    } else {
+        priceResult.innerText = 'El cupón no es valido';
+        return;
+    }
+
+    
+
+   
+   
+   ------USANDO CONDICIONAL if---------
+
+   if (coupon == 'fabian_es_spiderman') {
         discount = 30;
     } else if (coupon == 'no_le_digas_a_nadie') {
         discount = 20;
@@ -28,7 +90,9 @@ function calcularPrecioConDescuento() {
         priceResult.innerText = 'El cupón no es valido';
         return;
     }
-/*
+
+
+    -----CON SENTENCIA SWITCH-------
     switch (coupon) {
         case 'fabian_es_spiderman':
             discount = 30;
