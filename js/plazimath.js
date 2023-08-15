ecd8c4a192e58184917914947ca8c4bd194c6a3f
Fabián Huertas <miloh9318@hotmail.com>
2023-08-14 19:35:15 -0500

    agregando metodo reduce

--- js/plazimath.js
+++ js/plazimath.js
@@ -1,11 +1,16 @@
 function calcularPromedio(array) {
     //sumar todos los elementos del array / cantidad de elementos
-    let sumaArray = 0;
+    // let sumaArray = 0;
 
-    for (let i = 0; i < array.length; i++) {
-        sumaArray = sumaArray + array [i];
+    // for (let i = 0; i < array.length; i++) {
+    //     sumaArray = sumaArray + array [i];
+    // }
+
+    function sumarElementos (valorAcumulado, nuevoValor ) {
+        return valorAcumulado + nuevoValor;
     }
 
+    const sumaArray = array.reduce(sumarElementos);
     const promedio = sumaArray / array.length;
     return promedio;
     console.log(promedio);
